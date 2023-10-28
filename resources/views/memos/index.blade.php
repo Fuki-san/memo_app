e9427bcda786a28af50488e2b4d03400ff2a4dcd
Fuki-san <ff1028mh@gmail.com>
2023-10-28 17:54:14 +0900

    aa

---
+++ resources/views/memos/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>memo index</title>
+</head>
+<body>
+    <h1>タイトル一覧</h1>
+    <ul>
+        @foreach ($memos as $memo)
+            <li><a href="{{ route('memos.show', $memo) }}">{{ $memo->title }}</a></li>
+        @endforeach
+    </ul>
+</body>
+</html>
